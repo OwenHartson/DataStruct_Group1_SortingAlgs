a430ad27f72abb28e6c748682942b0eef4b7e8ee
Chris Schneider <85539501+chris-p-schneider@users.noreply.github.com>
2024-04-15 22:08:13 -0400

    updated Makefile for additional flags; added global bools to toggle function switches; added function to calculate 1 minute run times and mean times; added more comment dividers, reformatted prints with iomanip

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CXXFLAGS = -g -O3 -Wpedantic -Wall -Wextra -Wmisleading-indentation -Wunused -Wuninitialized -Wshadow -Wconversion -std=c++20
+CXXFLAGS = -g -O3 -Wpedantic -Wall -Wextra -Wmisleading-indentation -Wunused -Wuninitialized -Wshadow -Wconversion -Wfloat-equal -Wparentheses -Wunreachable-code -std=c++20
 
 OBJS = main
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
+//==================================================
 //==================== INCLUDES ====================
+//==================================================
+
 #include <iostream>
+#include <iomanip>
 #include <chrono>
 #include <vector>
 #include <algorithm>
 
-//==================== NAMESPCAES ====================
+//==================================================
+//==================== NAMESPACES ==================
+//==================================================
+
 using namespace std;
 using namespace std::chrono;
 
-//==================== GLOBALS ====================
+//==================================================
+//==================== GLOBALS =====================
+//==================================================
+
 enum SORT_TYPES{STD_SORT       = 0,
                 BUBBLE_SORT    = 1,
                 MERGE_SORT     = 2,
@@ -18,10 +28,38 @@ enum SORT_TYPES{STD_SORT       = 0,
                 BUCKET_SORT    = 6 
                 };
 
-//==================== FORWARD FUNCTION DECL ====================
+//==================================================
+
+// true to find the one minute time for a sort
+// loop start/increment needs manually tweaked to find it in a reasonable time
+bool PRINT_MINUTE = false; // 📑
+
+// true to calculate an average sort time
+bool PRINT_AVERAGES = true; // 📑
+
+// how many vectors to average
+const int NUM_VECTORS = 10;
+
+// the size of the vectors to average
+const int SIZES[] = { 32, 64, 128, 256, 512, 1024 };
+
+// the sort types to use in averaging; note std sort is not needed here
+const SORT_TYPES ENUMS[] = { BUBBLE_SORT, MERGE_SORT, 
+                             INSERTION_SORT, SELECTION_SORT, 
+                             QUICK_SORT, BUCKET_SORT };
+
+const string LINE = "------------------------------------------------------------";
+
+//==================================================
+//============= FUNCTION DECLARATIONS ==============
+//==================================================
+
 void fillVector(int fillValue, vector<int> & vec);
 void determineSort(SORT_TYPES name, vector<int> vec);
 void printVector(vector<int> vec);
+
+long int averageSort(SORT_TYPES name, vector<int> vec);
+bool minuteSort(SORT_TYPES name, vector<int> vec);
 
 void bubbleSort(vector<int> vec);
 
@@ -37,9 +75,113 @@ void bucketSort(vector<int> vec);
 void quickSort(vector<int> vec, int, int);
 int partition(vector<int> vec, int, int);
 
-//==================== MAIN ====================
-int main(){
-    for(int i = 100; i <= 1000; i += 100){
+//==================================================
+//==================== MAIN ========================
+//==================================================
+
+int main() {
+
+    if (PRINT_MINUTE) {
+        bool sortComplete = false;        
+        //======================================================
+        // These need manually adjusted to cut down time/memory
+        //======================================================
+        /* Since each iteration takes ~1 minute,
+           I'm starting with a large increment then refining
+           the initial value and reducing the increment 
+           until I can get a result very close to 60s.
+        */
+        int initial   = 750'000'000;
+        int increment = 10'000'000;
+
+        for (int j = initial; not sortComplete; j+=increment) {
+            cout << "j = " << j << endl;
+            vector<int> unsortedVector;
+            // cout << "max size: " << unsortedVector.max_size() << endl; // 2.3Q elements
+            fillVector(j, unsortedVector);
+            //===========================================================
+            // Ranked approximately slowest to fastest
+            //===========================================================
+            // sortComplete = minuteSort(BUBBLE_SORT,    unsortedVector);
+            // sortComplete = minuteSort(QUICK_SORT,     unsortedVector);
+            // sortComplete = minuteSort(SELECTION_SORT, unsortedVector);
+            // sortComplete = minuteSort(INSERTION_SORT, unsortedVector);
+            // sortComplete = minuteSort(MERGE_SORT,     unsortedVector);
+            // sortComplete = minuteSort(BUCKET_SORT,    unsortedVector);
+            sortComplete = minuteSort(STD_SORT,       unsortedVector);
+
+            if (not sortComplete) {
+                unsortedVector.clear();
+            }
+
+        }
+    }
+    
+    //==================================================
+    
+    if (PRINT_AVERAGES) {
+        cout << LINE << endl;
+        cout << "Finding the mean of " << NUM_VECTORS << " vectors..." << endl;
+        cout << LINE << endl;
+        // for each sort
+        for (int e = 0; e < 6; e++) {
+            // for each size
+            for (int s = 0; s < 6; s++) {
+                vector<long int> stdTimes;
+                vector<long int> sortTimes;
+
+                // average N>=10 vectors and compare to std sort
+                for (int i = 0; i < NUM_VECTORS; i++) {
+                    vector<int> unsortedVector;
+                    fillVector(SIZES[s], unsortedVector);
+                    stdTimes.push_back(averageSort(STD_SORT, unsortedVector));
+                    sortTimes.push_back(averageSort(ENUMS[e], unsortedVector));
+                }
+                // std sort results
+                long int stdSum = 0;
+                for (long int j: stdTimes) {
+                    stdSum += j;
+                }
+                cout << setw(14) << right << "Std Sort" << ":\t";
+                cout << "mean = " << setw(8) << right << stdSum/stdTimes.size() << " nanoseconds";
+                cout << "\titems = " << setw(4) << left << SIZES[s] << endl;
+
+                // test sort results
+                string sortName = "";
+                switch (ENUMS[e]) {
+                    case BUBBLE_SORT:
+                        sortName = "Bubble Sort"; break;
+                    case MERGE_SORT:
+                        sortName = "Merge Sort"; break;
+                    case INSERTION_SORT:
+                        sortName = "Insertion Sort"; break;
+                    case SELECTION_SORT:
+                        sortName = "Selection Sort"; break;
+                    case QUICK_SORT:
+                        sortName = "Quick Sort"; break;
+                    case BUCKET_SORT:
+                        sortName = "Bucket Sort"; break;
+                    case STD_SORT:
+                    default:
+                        sortName = "Std Sort"; break;
+                }
+                long int sortSum = 0;
+                for (long int k: sortTimes) {
+                    sortSum += k;
+                }
+                cout << setw(14) << right << sortName << ":\t";
+                cout << "mean = " << setw(8) << right << sortSum/sortTimes.size() << " nanoseconds";
+                cout << "\titems = " << setw(4) << left << SIZES[s] << endl;
+            }
+
+        }
+
+    }
+    
+    cout << LINE << endl;
+    //==================================================
+    
+    for (int i = 100; i <= 1000; i += 100) {
         int sortingSize = i;
         vector<int> unsortedVector;
         fillVector(sortingSize, unsortedVector);
@@ -53,10 +195,14 @@ int main(){
         determineSort(BUCKET_SORT, unsortedVector);
     }
 
+    cout << LINE << endl;
     return 0;
 }
 
-//==================== PRINT VECTOR ====================
+//==================================================
+//================ PRINT VECTOR ====================
+//==================================================
+
 void printVector(vector<int> vec){
     for(auto x : vec){
         cout << x << " ";
@@ -66,14 +212,105 @@ void printVector(vector<int> vec){
     return;
 }
 
-//==================== DETERMINE SORT ====================
+//==================================================
+//================ AVERAGE SORTS ===================
+//==================================================
+
+long int averageSort(SORT_TYPES name, vector<int> vec) {
+    string sortName = "";
+    auto start = high_resolution_clock::now();
+    vector<int> tempVecMerge = vec;
+
+    switch(name){
+        case STD_SORT:
+            sort(vec.begin(), vec.end());
+            sortName = "Std Sort"; break;
+        case BUBBLE_SORT:
+            sortName = "Bubble Sort";
+            bubbleSort(vec); break;
+        case MERGE_SORT:
+            sortName = "Merge Sort";
+            mergeSort(tempVecMerge, 0, static_cast<int>(tempVecMerge.size() - 1)); break;
+        case INSERTION_SORT:
+            sortName = "Insertion Sort";
+            insertionSort(vec); break;
+        case SELECTION_SORT:
+            sortName = "Selection Sort";
+            selectionSort(vec); break;
+        case QUICK_SORT:
+            sortName = "Quick Sort";
+            quickSort(vec, 0, static_cast<int>(vec.size() - 1)); break;
+        case BUCKET_SORT:
+            sortName = "Bucket Sort";
+            bucketSort(vec); break;
+        default: break;
+    }
+
+    auto finish   = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(finish - start);
+
+    return duration.count();
+}
+
+//==================================================
+//=============== 1 MINUTE SORT ====================
+//==================================================
+
+bool minuteSort(SORT_TYPES name, vector<int> vec) {
+    string sortName = "";
+    auto start = high_resolution_clock::now();
+    vector<int> tempVecMerge = vec;
+
+    switch(name){
+        case STD_SORT:
+            sort(vec.begin(), vec.end());
+            sortName = "Std Sort"; break;
+        case BUBBLE_SORT:
+            sortName = "Bubble sort";
+            bubbleSort(vec); break;
+        case MERGE_SORT:
+            sortName = "Merge sort";
+            mergeSort(tempVecMerge, 0, static_cast<int>(tempVecMerge.size() - 1)); break;
+        case INSERTION_SORT:
+            sortName = "Insertion sort";
+            insertionSort(vec); break;
+        case SELECTION_SORT:
+            sortName = "Selection sort";
+            selectionSort(vec); break;
+        case QUICK_SORT:
+            sortName = "Quick sort";
+            quickSort(vec, 0, static_cast<int>(vec.size() - 1)); break;
+        case BUCKET_SORT:
+            sortName = "Bucket sort";
+            bucketSort(vec); break;
+        default:
+            break;
+    }
+
+    auto finish   = high_resolution_clock::now();
+    auto duration = duration_cast<seconds>(finish - start);
+    auto minute   = seconds(60);
+
+    cout << setw(14) << right << sortName << ":\t";
+    cout << setw(8) << right << duration.count() << " seconds" << "\t";
+    cout << setw(4) << right << vec.size() << " items" << endl;
+
+    if (duration.count() >= minute.count()) {
+        return true;
+    }
+    return false;
+}
+
+//==================================================
+//================ DETERMINE SORT ==================
+//==================================================
+
 void determineSort(SORT_TYPES name, vector<int> vec){
-    string sortName;
+    string sortName = "";
     auto start = high_resolution_clock::now();
     ///SEE BELOW FOR tempVecMerge/////
     vector<int> tempVecMerge = vec;
     /////////////////////////////////
-
 
     switch(name){
         case STD_SORT:
@@ -112,12 +349,17 @@ void determineSort(SORT_TYPES name, vector<int> vec){
 
     auto finish = high_resolution_clock::now();
     auto duration = duration_cast<nanoseconds>(finish - start);
-    cout << sortName << "\t" << duration.count() << " nanoseconds" << "\t\t" << vec.size() << " items" << endl;
+    cout << setw(14) << right << sortName << ":\t";
+    cout << setw(8) << right << duration.count() << " nanoseconds" << "\t";
+    cout << setw(4) << right << vec.size() << " items" << endl;
 
     return;
 }
 
-//==================== QUICK SORT ====================
+//==================================================
+//================== QUICK SORT ====================
+//==================================================
+
 int partition(vector<int> vec, int start, int end){
     int pivot = vec[start];
     int count = 0;
@@ -150,6 +392,8 @@ int partition(vector<int> vec, int start, int end){
     return pivotIndex;
 }
 
+//==================================================
+
 void quickSort(vector<int> vec, int start, int end){
     vector<int> items = vec;
     if(start >= end){ return; }
@@ -161,7 +405,10 @@ void quickSort(vector<int> vec, int start, int end){
     return;
 }
 
-//==================== BUBBLE SORT ====================
+//==================================================
+//================= BUBBLE SORT ====================
+//==================================================
+
 void bubbleSort(vector<int> vec){
     long unsigned int step, i;
 
@@ -185,8 +432,10 @@ void bubbleSort(vector<int> vec){
     return;
 }
 
+//==================================================
+//================== MERGE SORT ====================
+//==================================================
 
-//==================== MERGE SORT ====================
 void mergeSort(vector<int>& vec, int p, int r) {
     if (p < r) {
         int q = p + (r - p) / 2;
@@ -197,6 +446,8 @@ void mergeSort(vector<int>& vec, int p, int r) {
         merge(vec, p, q, r);
     }
 }
+
+//==================================================
 
 void merge(vector<int>& vec, int p, int q, int r) {
     int n1 = q - p + 1;
@@ -236,7 +487,10 @@ void merge(vector<int>& vec, int p, int q, int r) {
     }
 }
 
-//==================== INSERTION SORT ====================
+//==================================================
+//================= INSERTION SORT =================
+//==================================================
+
 void insertionSort(vector<int> vec){
     for (size_t i = 1; i < vec.size(); ++i) {
         int k = vec[i];
@@ -251,7 +505,10 @@ void insertionSort(vector<int> vec){
     return;
 }
 
-//==================== SELECTION SORT ====================
+//==================================================
+//============== SELECTION SORT ====================
+//==================================================
+
 void selectionSort(vector<int> vec){
     for (size_t i = 0; i < vec.size() - 2; ++i) {
         size_t minIndex = i;
@@ -270,7 +527,10 @@ void selectionSort(vector<int> vec){
     return;
 }
 
-//==================== BUCKET SORT ====================
+//==================================================
+//================= BUCKET SORT ====================
+//==================================================
+
 void bucketSort(vector<int> vec) {
     if (vec.empty()){
         return;
@@ -293,8 +553,10 @@ void bucketSort(vector<int> vec) {
     }
 }
 
+//==================================================
+//================= FILL VECTOR ====================
+//==================================================
 
-//==================== FILL VECTOR ====================
 void fillVector(int fillValue, vector<int> & vec){
     vec.erase(vec.begin(), vec.end());
 
@@ -304,3 +566,5 @@ void fillVector(int fillValue, vector<int> & vec){
     }
     return;
 }
+
+//==================================================
